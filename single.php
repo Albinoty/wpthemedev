527c949edc40bdda782e9e5a9b64de22ba7dfa99
albinoty <albinotnoty@gmail.com>
2019-08-12 16:22:24 +0200

    Ajout d'une div.post-content pour affocher le contenu de l'article. Le contenu est affiché grace à la fonction the_content(). Cependant cela n'affichera rien ca il faudra d'abord entourer le tout de la célébre boucle de wordpress. Cette boucle est le while(have_posts()). Nous avons maintenat accès aux données du post.

--- single.php
+++ single.php
@@ -2,9 +2,14 @@
     get_header();
 ?>
 <div class="container single-post-container">
+    <?php while(have_posts()) : the_post(); ?>
     <h2 class="text-center text-uppercase text-secondary mb-0">
         <?php the_title(); ?>
     </h2>
+    <div class="post-content">
+        <?php the_content(); ?>
+    </div>
+    <?php endwhile; ?>
 </div>
 <?php 
     get_footer(); 
