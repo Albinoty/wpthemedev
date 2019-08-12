4ccf46c481bd20c20aac40adab9ccf612db41f0c
albinoty <albinotnoty@gmail.com>
2019-08-12 11:53:01 +0200

    Ajout de la section food via get_template_part()

--- index.php
+++ index.php
@@ -7,57 +7,13 @@
     get_template_part('templates/info');
     //On va chercher la partie ville
     get_template_part('templates/ville');
+    //On va chercher la partie food
+    get_template_part('templates/food');
 
 ?>
     
         
-        
-        
-        <!-- debut Food + slide-->
-        <div class="border row pt-2 pb-2 fond">
-            <div class="col-sm-6  d-flex align-items-center">
-                <div id="foodSlide" class="carousel slide" data-ride="carousel" data-interval="0">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="http://localhost:8080/wp-content/themes/albi/images/food/burek.jpg" class="d-block w-100" alt="burek">
-                            <div class="intro carousel-caption d-none d-md-block">
-                                <p>1</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="http://localhost:8080/wp-content/themes/albi/images/food/fllija.jpg" class="d-block w-100" alt="fllija">
-                            <div class=" intro carousel-caption d-none d-md-block">
-                                <p>2</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="http://localhost:8080/wp-content/themes/albi/images/food/pasul.jpg" class="d-block w-100" alt="pasul">
-                            <div class=" intro carousel-caption d-none d-md-block">
-                                <p>3</p>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#foodSlide" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true">
-                        <span class="sr-only">Pr&eacute;c&eacute;dent</span></span>                    
-                    </a>
-                    <a class="carousel-control-next" href="#foodSlide" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true">
-                        <span class="sr-only">Suivant</span></span>                    
-                    </a>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <h2>Ushqim tradicional Shqiptar / <i>Nourriture traditionelle albanais</i></h2>
-                <h3>1.Burek</h3>
-                <p class="pl-2">Un börek, burek, ou beurek est une variété de pâtisserie salée</p>
-                <h3>2.Flija</h3>
-                <p class="pl-2">La flia (aussi connue sous le nom de « fli » ou « flija ») est un plat de la cuisine albanaise. Elle se compose de plusieurs couches de crêpes couvertes avec de la crème</p>
-                <h3>3.Pasul</h3>
-                <p class="pl-2">Le pasul est une soupe avec un m&eacute;lange d'haricots blancs, sauce toamte, de viande boeuf le tout en rajoutant du paprika et du piment</p>
-            </div>
-        </div>
-        <!-- fin Food + slide -->
+    
         <!-- Debut mer + slide -->
         <div class="row fond">
             <div class="col-sm-6">
