95c97144a4b4d4d443f4722de54af23f39b52997
albinoty <albinotnoty@gmail.com>
2019-08-12 11:41:42 +0200

    Ajout d'un fichier intro.php dans le dossier templates qui va contenir tous les fichiers templates de wordpres. Sa sera ainsi la structure a adapter. On appele via get_template_part() et indiquer quel fichier 'templates/fichier'

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
     <!-- Bootstrap script -->
     <!-- Jquery -->
-    <script src="http://localhost:8080/wp-content/themes/albi/node_modules/jquery/dist/jquery.js"></script>
+    <script src="http://localhost:8080/wp-content/themes/albi/node_modules/ jquery/dist/jquery.js"></script>
     <!-- Bootstrap Script -->
     <script src="http://localhost:8080/wp-content/themes/albi/node_modules/bootstrap/dist/js/bootstrap.js"></script>
 </body>
---
+++ templates/intro.php
@@ -0,0 +1,37 @@
+<section class="container card p-0">
+        <!-- Intro bg slide -->
+        <div class="row">
+            <div id="slideControler" class="col-sm carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="http://localhost:8080/wp-content/themes/albi/images/city/qender1.png" class="d-block w-100" alt="qender1">
+                        <div class="intro carousel-caption d-none d-md-block">
+                            <h1>Mir&euml; se erdhe</h1>
+                            <p><i>Bienvenue</i></p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="http://localhost:8080/wp-content/themes/albi/images/city/qender2.png" class="d-block w-100" alt="qender2">
+                        <div class="intro carousel-caption d-none d-md-block">
+                            <h1>Mir&euml; se erdhe</h1>
+                            <p><i>Bienvenue</i></p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="http://localhost:8080/wp-content/themes/albi/images/city/ksamil.jpg" class="d-block w-100" alt="ksamil">
+                        <div class="intro carousel-caption d-none d-md-block">
+                            <h1>Mir&euml; se erdhe</h1>
+                            <p><i>Bienvenue</i></p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#slideControler" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true">
+                    <span class="sr-only">Pr&eacute;c&eacute;dent</span></span>                    
+                </a>
+                <a class="carousel-control-next" href="#slideControler" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true">
+                        <span class="sr-only">Suivant</span></span>                    
+                </a>
+            </div>
+        </div>
