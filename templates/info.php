699e3995a5d68e7c7cf4ac4ee6ae90a1233a4321
albinoty <albinotnoty@gmail.com>
2019-08-12 15:21:10 +0200

    Ajout de la description du site de maniere dynamique dans info.php en utilisant get_bloginfo('description')

--- templates/info.php
+++ templates/info.php
@@ -4,7 +4,7 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/images/shqiponja.png" class="w-100" id="drapeau" alt="drapeau">
             </div>
             <div class="col-sm-8">       
-                    <p>Bienvenue dans notre site. Nous allons vous presenter la culture albanaise &agrave; travers nos slides. Pour toute information, veuillez vous redirigez dans l'onglet Contact ou <a class="rediriction" href="info/contact.html">Cliquer ici.</a></p>
+                <p><?php echo get_bloginfo('description'); ?></p>
             </div>
         </div>
         <!-- fin Info  -->
