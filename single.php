fdf2956ba6ac1b4fd1f68955b225b6ec342064f3
albinoty <albinotnoty@gmail.com>
2019-08-12 16:10:33 +0200

    Creation d'un article avec un titre et un contenu. Lorsque nous visionons cette article, nous somme rediriger sur la page index.php. Nous avons creer un fichier single.php qui reprend le header.php et le footer.php. Entre les deux se trouve une div avec une class single-post-container pour pouvoir appliquer du css dans la personnalisation avec un margin-top: 106px;. A l'interieur une div, h2 avec comme texte le titre de l'article que on l'affiche grace a la fonction the_title(). L'article a également une catégorie logo

---
+++ single.php
@@ -0,0 +1,11 @@
+<?php
+    get_header();
+?>
+<div class="container single-post-container">
+    <h2 class="text-center text-uppercase text-secondary mb-0">
+        <?php the_title(); ?>
+    </h2>
+</div>
+<?php 
+    get_footer(); 
+?>
