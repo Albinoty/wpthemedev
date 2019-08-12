f5dd5a4a130dd8c9ab160017efb8a57b43de5414
albinoty <albinotnoty@gmail.com>
2019-08-12 10:27:06 +0200

    Ajout du dossier vendor qui contient le js, css et dans index le lien de bootstrap en chemin absolu -> http://localhost:8080/wp-content/themes/albi/ suivi du dossier

--- index.php
+++ index.php
@@ -5,8 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+
+    <link rel="stylesheet" href="http://localhost:8080/wp-content/themes/albi/vendor/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Mon premier site avec theme perso</h1>
+    
+
+    <h1 class="bg-primary">Mon premier site avec theme perso</h1>
+    <header class="container">
+        <div class="row">
+            <div class="col-6">lol</div>
+            <div class="col-6">aa</div>
+        </div>
+    </header>
+    
+    
 </body>
 </html>
