b19e2db549732ab3e5ac4e22014fae135609ed52
albinoty <albinotnoty@gmail.com>
2019-08-12 11:51:16 +0200

    Ajout de la section ville via get_template_part()

--- index.php
+++ index.php
@@ -5,60 +5,14 @@
     get_template_part('templates/intro');
     //On va chercher la partie info
     get_template_part('templates/info');
+    //On va chercher la partie ville
+    get_template_part('templates/ville');
 
 ?>
     
         
         
-        <!-- debut ville + slide -->
-        <div class="row fond">
-            <div class="col-sm-6">
-                <h2>Qend&euml;r / <i>Ville</i></h2>
-                <div>
-                    <h3>1.Tiran&euml;</h3>
-                    <p class="pl-2">Tirana (Tiran&euml; en albanais) est la capitale et la plus peuplée des villes de l'Albanie. Elle est le centre du district de Tirana, un des 12 districts constituants de l'Albanie.</p>
-                </div>
-                <div>
-                    <h3>2.Shkod&euml;r</h3>
-                    <p class="pl-2">Shkodra (Shkod&euml;r en albanais) est une municipalité du nord-ouest de l'Albanie, la ville principale de la région et au bord du lac du même nom, le plus grand lac des Balkans (d'une surface de 370 km<sup>2</sup>), près des fleuves Drin, Kir et Buna, où passe la frontière actuelle de l'Albanie avec le Monténégro. Elle abrite le Château de Shkodra, qui se trouve à une hauteur de 130 mètres.</p>
-                </div>
-                <div>
-                    <h3>3.Prishtin&euml;</h3>
-                    <p class="pl-2">Pristina (Prishtin&euml; en albanais)une ville avec une population majoritairement albanaise, aux côtés d’autres communautés plus petites. Avec une population municipale de 204 721 habitants (2016), Pristina est la deuxième plus grande ville du monde avec une population majoritairement albanophone, après l'Albanie, la capitale Tirana .</p>
-                </div>
-            </div>
-            <div id="qenderSlide" class="col-sm-6 carousel slide d-flex align-items-center" data-ride="carousel" data-interval="0">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="http://localhost:8080/wp-content/themes/albi/images/city/qender1.png" class="d-block w-100" alt="qender1">
-                            <div class="intro carousel-caption d-non d-md-block">
-                                <p>1</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="http://localhost:8080/wp-content/themes/albi/images/city/shkodra.png" class="d-block w-100" alt="shkodra">
-                            <div class="intro carousel-caption d-non d-md-block">
-                                <p>2</p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="http://localhost:8080/wp-content/themes/albi/images/city/prishtine.png" class="d-block w-100" alt="prishtine">
-                            <div class="intro carousel-caption d-none d-md-block">
-                                <p>3</p>
-                            </div>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#qenderSlide" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true">
-                        <span class="sr-only">Pr&eacute;c&eacute;dent</span></span>                    
-                    </a>
-                    <a class="carousel-control-next" href="#qenderSlide" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true">
-                            <span class="sr-only">Suivant</span></span>                    
-                    </a>
-                </div>
-        </div>
-        <!-- fin ville + slide -->
+        
         <!-- debut Food + slide-->
         <div class="border row pt-2 pb-2 fond">
             <div class="col-sm-6  d-flex align-items-center">
