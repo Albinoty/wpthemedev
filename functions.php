eb192d13e8d8c0ebd27e43db4149cf7f69349a27
albinoty <albinotnoty@gmail.com>
2019-08-13 15:58:53 +0200

    refactoring de la fonction pour ajouter des class au balises <a> du menu

--- functions.php
+++ functions.php
@@ -13,27 +13,7 @@
 
     require_once(INCLUDE_DIR . '/menu.php');
 
-    /**
-    *
-    * Fonction qui ajoute des attributs au balise <a> des nav_menu
-    *
-    * @param [type] $att
-    * @param [type] $item
-    * @param [type] $args
-    * @return void
-    *
-    */
-
-    function ajout_menu_a_class($att, $item, $args){
-        $class = 'nav-link rediriction';
-        $atts['class'] = $class;
-        return $atts;
-    }
-
-    // Ajout d'un écouteur d'événement de type filtre qui nous permet de changer les attributs des balises <a>
-    // les add_action et add_filter peuvent avoit jusqu'à 4 paramêtre. Le 3ème pour l'ordre d'execution et le 4 ème pour le nombre de parammètre qui seront passer à la fonction callback
-
-    add_filter('nav_menu_link_attributes','ajout_menu_a_class',10,3);
+    
     /**
      * Ajout la fonctionnalité d'un ajout d'image pour les posts pour ce thème ci
      *
--- includes/menu.php
+++ includes/menu.php
@@ -14,8 +14,31 @@ class MgMenu{
             register_nav_menu('main-menu', 'Menu principal dans le header');
 
         }
+
+        /**
+        *
+        * Fonction qui ajoute des attributs au balise <a> des nav_menu
+        *
+        * @param [type] $att
+        * @param [type] $item
+        * @param [type] $args
+        * @return void
+        *
+        */
+
+        public static function ajout_menu_a_class($att, $item, $args){
+            $class = 'nav-link rediriction';
+            $atts['class'] = $class;
+            return $atts;
+        }
+
     }
-    
 
     add_action('after_setup_theme', [MgMenu::class,'register_main_menu']);
+
+    // Ajout d'un écouteur d'événement de type filtre qui nous permet de changer les attributs des balises <a>
+    // les add_action et add_filter peuvent avoit jusqu'à 4 paramêtre. Le 3ème pour l'ordre d'execution et le 4 ème pour le nombre de parammètre qui seront passer à la fonction callback
+    add_filter('nav_menu_link_attributes',[MgMenu::class,'ajout_menu_a_class'],10,3);
+
+
 ?>
