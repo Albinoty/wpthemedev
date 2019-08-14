db64af2c01a50094dcaf8ee67db29beda0ba3232
albinoty <albinotnoty@gmail.com>
2019-08-14 09:16:57 +0200

    Ajout d'un fichier partials contenant ta grid des posts portfolio que l'on importe grâce à get_template_part();

--- page.php
+++ page.php
@@ -19,30 +19,7 @@ get_header();
             endwhile;
         ?>
     </div>
-    <div class="portfolio-link row">
-        <?php
-            // Nous allons faire en sorte d'aller chercher tout les articles pour les affichers sur la page
-            // Pour cela nous allons utiliser la class WP_Querry
-            // https://developer.wordpress.org/reference/classes/wp_query/
-            $args = [
-                'post_type' => 'post'
-            ];
-            $query = new WP_Querry($args);
-            while ($query->have_posts()): $query->the_post();
-
-        ?>
-        <div class="col-md-4">
-            <a href="<?php the_permalink(); ?>">
-                <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="img-fluid">
-            </a>
-            <h3 class="text-center">
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_title(); ?>
-                </a>
-            </h3>
-        </div>
-        <?php endwhile ?>
-    </div>
+    <?php get_template_part('templates/partials/grid-portfolio'); ?>
 </div>
 <?php 
 
---
+++ templates/partials/grid-portfolio.php
@@ -0,0 +1,24 @@
+    <div class="portfolio-link row">
+        <?php
+            // Nous allons faire en sorte d'aller chercher tout les articles pour les affichers sur la page
+            // Pour cela nous allons utiliser la class WP_Querry
+            // https://developer.wordpress.org/reference/classes/wp_query/
+            $args = [
+                'post_type' => 'post'
+            ];
+            $query = new WP_Querry($args);
+            while ($query->have_posts()): $query->the_post();
+
+        ?>
+        <div class="col-md-4">
+            <a href="<?php the_permalink(); ?>">
+                <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="img-fluid">
+            </a>
+            <h3 class="text-center">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                </a>
+            </h3>
+        </div>
+        <?php endwhile ?>
+    </div>  
