d49001ff36624d01256104e6c2629422d9f84610
albinoty <albinotnoty@gmail.com>
2019-08-14 09:13:41 +0200

    Ajout d'une query afin de pouvoir récuperer tout les post (article) et les afficher dans la page portfilio. On découvre la class WP_Query ainsi que la fonction wp_reset_query()

--- page.php
+++ page.php
@@ -3,20 +3,48 @@
 get_header();
 
 ?>
-
-<?php while (have_posts()) : the_post(); ?>
-
 <div class="page-banner" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
-    <h1><?php the_title(); ?></h1>
+    <?php while (have_posts()) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+    <?php endwhile; ?>
 </div>
 <div class="container single-post-container">
     <div class="post-content">
-        <?php the_content(); ?>
+        <!-- la fonction wp_reset_query() permet de réinitialiser les valeurs $post en fonction du context -->
+        <?php 
+            wp_reset_query();
+
+            while(have_posts()): the_post; 
+            the_content(); 
+            endwhile;
+        ?>
+    </div>
+    <div class="portfolio-link row">
+        <?php
+            // Nous allons faire en sorte d'aller chercher tout les articles pour les affichers sur la page
+            // Pour cela nous allons utiliser la class WP_Querry
+            // https://developer.wordpress.org/reference/classes/wp_query/
+            $args = [
+                'post_type' => 'post'
+            ];
+            $query = new WP_Querry($args);
+            while ($query->have_posts()): $query->the_post();
+
+        ?>
+        <div class="col-md-4">
+            <a href="<?php the_permalink(); ?>">
+                <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="img-fluid">
+            </a>
+            <h3 class="text-center">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                </a>
+            </h3>
+        </div>
+        <?php endwhile ?>
     </div>
 </div>
 <?php 
-
-    endwhile; 
 
     get_footer();
 
